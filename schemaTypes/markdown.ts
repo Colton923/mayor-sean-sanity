27868fe7812530160f531b8a073f99d502004fff
Colton <colton923@gmail.com>
2025-01-14 11:23:17 -0600

    .

--- schemaTypes/markdown.ts
+++ schemaTypes/markdown.ts
@@ -1,7 +1,7 @@
 import {defineField, defineType} from 'sanity'
 
 export const markdown = defineType({
-  name: 'markdown',
+  name: 'markdownSchema',
   title: 'Markdown',
   type: 'document',
   fields: [
